e07001f67dbdb325b87362c84e17215058416244
torpedonosec80 <firsovmikhael@gmail.com>
2020-05-13 13:42:44 +0300

    4 commit

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,11 @@ class Category(models.Model):
     """Category model"""
     name = models.CharField("Имя", max_length=100)
     slug = models.SlugField("url", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
