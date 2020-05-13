80eb0310bebc79ab3f5ebf824281de1d7b59351d
torpedonosec80 <firsovmikhael@gmail.com>
2020-05-13 13:18:46 +0300

    3 commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField("Имя", max_length=100)
+    slug = models.SlugField("url", max_length=100)
